993c77850bc6c7af5c437e308886340fa6164f80
dwiilhammaulana <1123150008@global.ac.id>
2025-11-01 22:50:06 +0700

    memanggil splash_screen1

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+             
+            ),
     );
   }
 }
