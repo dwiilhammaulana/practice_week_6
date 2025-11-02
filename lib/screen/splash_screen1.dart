5d5368daac3525358938af0273d82f1fdc796412
dwiilhammaulana <1123150008@global.ac.id>
2025-11-02 09:34:44 +0700

    buat welcome & sub title

--- lib/screen/splash_screen1.dart
+++ lib/screen/splash_screen1.dart
@@ -17,18 +17,30 @@ class Screen1 extends StatelessWidget {
         // mulai dari warna terang diawal
         // Membuat Button Continue
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 50),
-            Container(
-              width: 250,
-              height: 250,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Colors.blue,
-              ),
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+          const SizedBox(height: 25),
+          Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: const Color.fromARGB(255, 104, 116, 104),
+              image: DecorationImage(image: AssetImage("assets/images/welcome.png"),
+              fit: BoxFit.cover,
+              )
             ),
-          ],
+          ),
+          SizedBox(height: 20),
+          Text("Welcome",style: TextStyle(
+            fontSize: 30,
+            color: Colors.black
+          )),
+          Text("Bersiap menghadapi tantangan? \n Temukan solusi survival terbaik di sini.",style: TextStyle(
+            fontSize: 15,
+            color: Colors.black
+          )),
+         ], 
         ),
       ),
     );
