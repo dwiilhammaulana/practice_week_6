f7d88846008ea19f9d8a840aa7e0e51955af94de
dwiilhammaulana <1123150008@global.ac.id>
2025-11-02 13:40:40 +0700

    enabelborder & foccuss border

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,22 +6,55 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            body: SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                child: const Column(
-                  children: [
-                    SizedBox(height: 10,),
-                    Icon(
-                      Icons.lock_outline,
-                      size: 80,
-                      color: Colors.green,
-                    ),
-                    SizedBox(height: 10,)
-                  ],
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: const Column(
+              children: [
+                SizedBox(height: 10),
+                Icon(Icons.lock_outline, size: 80, color: Colors.green),
+                SizedBox(height: 20),
+                Text(
+                  "Selamat Datang!",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
                 ),
-              ),
+                Text(
+                  "Silahkan Login untuk melanjutkan.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 12, color: Colors.green),
+                ),
+                SizedBox(height: 20),
+                SizedBox(height: 20),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email_outlined),
+                    labelText: 'Input email',
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25)),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25)),
+                      borderSide: BorderSide(
+                        color: Colors.green, // warna saat fokus
+                        width: 2, // ketebalan garis saat fokus
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 10),
+              ],
             ),
-          );
+          ),
+        ),
+      ),
+    );
   }
 }
